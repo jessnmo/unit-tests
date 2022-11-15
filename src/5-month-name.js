653e360b1eae84a37cee707f2d4bcf008745e954
jessnmo <jessicamo92@gmail.com>
2022-11-15 15:06:54 +0100

    1-3-5

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,23 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
-]
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
-}
+	const number = monthNumber - 1;
+	if (monthNumber <= 0 || monthNumber > 12) {
+		return null;
+	} else {
+		return months[number];
+	}
+};
--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,15 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+	const type = typeof stringArrayOrObject;
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
-  }
-
-  return false
-}
+	if (type === string) {
+		if (stringArrayOrObject === '') {
+			return true;
+		}
+	} else if (type === object) {
+		if (stringArrayOrObject === {}) return true;
+	} else if (type === {}) {
+		return true;
+	} else return false;
+	/* console.log(type);
+	return type.length === 0 ? true : false; */
+};
